c4fbf1ac584cd4287dc4e78d9b4103c1e2087ea8
Yerim998 <juin3062@gmail.com>
2025-06-17 12:52:40 +0900

    signup.js입력

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import router from "./router.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  if (!location.hash) {
+    location.hash = "/";
+  }
   router();
 });
---
+++ js/pages/SignUp.js
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return `
+    <section class="signup">
+      <h1>회원가입 페이지</h1>
+    </section>
+  `;
+}
