3aad1ec166cb8ae060c93d7949e60abc04c9890d
Yerim998 <juin3062@gmail.com>
2025-06-17 15:37:38 +0900

    로그인페이지 tap창 구현

--- js/pages/LoginPage.js
+++ js/pages/LoginPage.js
@@ -6,30 +6,27 @@ export default function LoginPage() {
 			<img src="./assets/getti.svg" alt="getti" />
 			</a>
     </h1>
+
+
     <div class="box">
       <div class="tab">
-        <button class="active">구매회원 로그인</button>
-        <button>판매회원 로그인</button>
+        <button class="active" data-type="buyer">구매회원 로그인</button>
+        <button data-type="seller">판매회원 로그인</button>
       </div>
-      <form id="login-form">
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="비밀번호" />
-        <button type="submit">로그인</button>
-      </form>
-      <div class="footer-link">
-        <a href="#/signup">회원가입</a> | <a href="#">비밀번호 찾기</a>
-      </div>
+
+			<div class="login-container">
+								<form id="login-form" class="login-form">
+								<input type="text" placeholder="아이디" />
+								<input type="password" placeholder="비밀번호" />
+								<button type="submit">로그인</button>
+								</form>
+			</div>
+
+
+				<div class="footer-link">
+				<a href="#/signup">회원가입</a> | <a href="#">비밀번호 찾기</a>
+					</div>
     </div>
   </section>
-
-	  <script>
-      const tabButtons = document.querySelectorAll(".tab button");
-      tabButtons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-          tabButtons.forEach((b) => b.classList.remove("active"));
-          btn.classList.add("active");
-        });
-      });
-    </script>
 	`;
 }
