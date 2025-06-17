e7e802d0a4e521fcccac4105b01d7c1107905486
Yerim998 <juin3062@gmail.com>
2025-06-17 14:01:19 +0900

    로고svg이미지 추가

--- js/pages/LoginPage.js
+++ js/pages/LoginPage.js
@@ -1,19 +1,22 @@
 export default function LoginPage() {
   return `
-  <section class="login">
-    <h1 class="logo">getti</h1>
-    <div class="tab">
-      <button class="active">구매회원 로그인</button>
-      <button>판매회원 로그인</button>
+  <section class="loginPage">
+    <h1 class="logo">
+      <a href="#/" aria-label="getti 홈으로 이동하기">getti</a>
+    </h1>
+    <div class="login-box">
+      <div class="tab">
+        <button class="active">구매회원 로그인</button>
+        <button>판매회원 로그인</button>
+      </div>
+      <form id="login-form">
+        <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
+        <button type="submit">로그인</button>
+      </form>
+      <div class="footer-link">
+        <a href="#/signup">회원가입</a> | <a href="#">비밀번호 찾기</a>
+      </div>
     </div>
-    <form>
-      <input type="text" placeholder="아이디" />
-      <input type="password" placeholder="비밀번호" />
-      <button type="submit">로그인</button>
-    </form>
-    <div class="footer-link">
-      <a href="#/signup">회원가입</a> | <a href="#">비밀번호 찾기</a>
-    </div>
-  </section>
-	`;
+  </section>`;
 }
