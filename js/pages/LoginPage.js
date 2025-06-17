cdfbfb08c44a1f26ad53310e38eb96cf59c43aa8
Yerim998 <juin3062@gmail.com>
2025-06-17 11:49:19 +0900

    로그인 페이지 html 입력

---
+++ js/pages/LoginPage.js
@@ -0,0 +1,19 @@
+export default function LoginPage() {
+  return `
+  <section calss="login">
+    <h1 class="logo">getti</h1>
+    <div class="tab">
+      <button class="active">구매회원 로그인</button>
+      <button>판매회원 로그인</button>
+    </div>
+    <form>
+      <input type="text" placeholder="아이디" />
+      <input type="password" placeholder="비밀번호" />
+      <button type="submit">로그인</button>
+    </form>
+    <div class="footer-link">
+      <a herf="#/signup">회원가입</a> | <a herf="#">비밀번호 찾기</a>
+    </div>
+  </section>
+	`;
+}
