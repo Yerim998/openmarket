d531f9d3745874456705ac4abdfc40adfccadc95
Yerim998 <juin3062@gmail.com>
2025-06-18 13:13:25 +0900

    회원가입 유효성 검사-선택자 추가, 비밀번호 체크아이콘 위치잡기

--- js/pages/SignUp.js
+++ js/pages/SignUp.js
@@ -19,22 +19,31 @@ export default function SignUp() {
         <form id="signup-form" class="signup-form" data-type="buyer">
           <label>아이디</label>
           <div class="id-check">
-            <input type="text" placeholder="" />
+            <input type="text" placeholder="" id="idInput" />
             <button type="button" class="check-btn">중복확인</button>
+						<p class="error-msg" id="id-error"></p>
           </div>
 
           <label>비밀번호</label>
-          <input type="password" placeholder="" />
+					<div class="pw-box">
+						<input type="password" placeholder=""  id="pwInput" />
+						<span class="pw-icon" id="pw-icon"></span>
+					</div>
+					<p class="error-msg" id="pw-error"></p>
 
           <label>비밀번호 재확인</label>
-          <input type="password" placeholder="" />
+					<div class="pw-box">
+          <input type="password" id="confirmPwInput" placeholder="" />
+					<span class="pw-icon" id="pw-icon"></span>
+					</div>
+					<p class="error-msg" id="confirm-error"></p>
 
           <label>이름</label>
-          <input type="text" placeholder="" />
+          <input type="text" placeholder=""  id="nameInput" />
 
           <label>휴대폰번호</label>
           <div class="phoneGroup">
-            <select class="phoneNum">
+            <select class="phoneNum" id="phoneFirst">
               <option value="010">010</option>
               <option value="011">011</option>
               <option value="016">016</option>
@@ -42,14 +51,16 @@ export default function SignUp() {
               <option value="018">018</option>
               <option value="019">019</option>
             </select>
-            <input type="text" placeholder="" />
-            <input type="text" placeholder="" />
+            <input type="text" placeholder="" / id="phoneMiddle" maxlength="4" >
+            <input type="text" placeholder="" id="phoneLast" maxlength="4" />
           </div>
+					<p class="error-msg" id="phone-error"></p>
+
 
           <div class="agreement">
             <label>
               <input type="checkbox" id="agree" />
-              게티마켓 이용약관 및 개인정보처리방침에 동의합니다.
+								게티마켓 이용약관 및 개인정보처리방침에 동의합니다.
             </label>
           </div>
 
--- js/pages/SignUpEvent.js
+++ js/pages/SignUpEvent.js
@@ -1,5 +1,19 @@
 export function setupSignUpEvent() {
-	const form = document.getElementById("signup-form");
+  const form = document.querySelector("#signup-from");
+  const idInput = form.querySelector("input[type='text']");
+  const idCheckBtn = form.querySelector(".check-btn");//중복확인버튼
+  const passwordInput = form.querySelector("input['type=password']");//비밀번호입력
+  const confirmPwInput = form.querySelectorAll("input[type='password']")[1]; //inputtext중 2번째(1)
+  const agreeCheckbox = form.querySelector("#agree");//체크박스
+	const submitBtn = form.querySelector(".submit-btn");//가입하기
 
-	const username = document.getElementById("username")
+	let idValid = false; //아이디 유효성
+
+	//중복확인
+	idCheckBtn.addEventListener("click", () => {
+		const idValue = idInput.value.trim();
+		if (idValue === "") {
+			
+		}
+	})
 }
