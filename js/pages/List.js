0d6e58e0498380f91e79b386c69b9b16ddb030ff
Yerim998 <juin3062@gmail.com>
2025-06-20 16:42:06 +0900

    list.js 기본 구조 잡기

--- js/pages/List.js
+++ js/pages/List.js
@@ -1,8 +1,63 @@
 export default function renderProductListPage() {
   return `
+	<main class="product-list-page">
+	<!-- 상단 배너 -->
+		<section class="banner-slider">
+		<button class="prev"></button>
+			<div class="slides">
+				<!-- <img src="배너1.jpg" alt="배너1">
+				<img src="배너2.jpg" alt="배너2">-->
+			</div>
+			<button class="next"></button>
+		</section>
+
+		<!-- 상품 목록 -->
     <section class="list">
-      <h1>리스트 (임시)</h1>
-      <p>글로벌 네비게이션 바 확인용</p>
+		<ul class="product-grid">
+			<li>
+				<article>
+				<img src=".." alt="상품명" />
+				<h2>상품명</h2>
+				<span>가격 및 정보</span>
+				</article>
+			</li>
+			<li>
+				<article>
+				<img src=".." alt="상품명" />
+				<h2>상품명</h2>
+				<span>가격 및 정보</span>
+				</article>
+			</li>
+			<li>
+				<article>
+				<img src=".." alt="상품명" />
+				<h2>상품명</h2>
+				<span>가격 및 정보</span>
+				</article>
+			</li>
+			<li>
+				<article>
+				<img src=".." alt="상품명" />
+				<h2>상품명</h2>
+				<span>가격 및 정보</span>
+				</article>
+			</li>
+			<li>
+				<article>
+				<img src=".." alt="상품명" />
+				<h2>상품명</h2>
+				<span>가격 및 정보</span>
+				</article>
+			</li>
+			<li>
+				<article>
+				<img src=".." alt="상품명" />
+				<h2>상품명</h2>
+				<span>가격 및 정보</span>
+				</article>
+			</li>
+    </ul>
     </section>
+		</main>
   `;
 }
